af4f35bb4484e69410ea8ff2c9cb392444739bb0
Kuwar Pratap Singh <guru75000@gmail.com>
2022-03-31 00:07:21 +0530

    update

--- src/components/prc.jsx
+++ src/components/prc.jsx
@@ -3,9 +3,9 @@ function PRC(){
     return(
 <> 
 <div className="wrap">
-<h2>Hello Sarru</h2>
+<h2>Hello Lucky</h2>
 <p>Hey! you wanna try your lucky number <br /> 
-Place wish on your Mind .</p>
+Place wish on your Mind nand then refresh.</p>
 <h4>your lucky number is :</h4> <h2>{Math.floor(Math.random()*10)}</h2>
 
 </div>
